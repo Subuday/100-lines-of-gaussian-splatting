b9cb445206a541dbcd07b8aa2102388ea0af3539
Maksym Sutkovenko <msutkovenko@readdle.com>
2023-12-27 23:39:31 +0200

    Add ability to init GaussianModel form SceneInfo

--- model.py
+++ model.py
@@ -1,5 +1,15 @@
+import numpy as np
+import torch
+from torch import nn
+
 
 class GaussianModel:
 
-    def __init__(self):
+    def __init__(self, device="cpu"):
+        self.param_point_cloud = None
+        self.device = device
         pass
+
+    def init_from_scene_info(self, scene_info):
+        point_cloud_tensor = torch.tensor(scene_info.point_cloud.points).to(self.device)
+        self.param_point_cloud = nn.Parameter(point_cloud_tensor.requires_grad_(True))
