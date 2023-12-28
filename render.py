2b763425cb92f6fe3c50e0cabe961b9c5a304a92
Maksym Sutkovenko <mr.gigant977@gmail.com>
2023-12-28 22:09:06 +0000

    Add stub for GaussianRasterizationSettings

--- render.py
+++ render.py
@@ -1,6 +1,18 @@
+import math
 import torch
+from diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianRasterizer
 from model import GaussianModel
+from camera import Camera
 
 
-def render(camera, model: GaussianModel, scaling_modifier=1.0, override_color=None):
+def render(camera: Camera, model: GaussianModel, scaling_modifier=1.0, override_color=None, device="cpu"):
+    # settings = GaussianRasterizationSettings(
+    #     image_width=camera.image_width,
+    #     image_height=camera.image_height,
+    #     tanfovx=math.tan(camera.FoVx * 0.5),
+    #     tanfovy=math.tan(camera.FoVy * 0.5),
+    #     bg=torch.tensor([0, 0, 0], dtype=torch.float32, device=device),
+    #     scale_modifier=scaling_modifier,
+    #     viewmatrix=camera.viewmatrix,
+    # )
     pass
